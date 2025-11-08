f76f76a210c409c65b26da6883b3cf103714b73e
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-11-08 00:09:25 +0000

    Create minikube-install.sh

---
+++ minikube-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+systemctl stop docker
+rm -rf /var/lib/docker
+rm /etc/containerd/config.toml
